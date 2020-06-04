0df0f20e4fd6f206f0494d7124e749f11bd2bb35
Yuchen Liu <yl6448@nyu.edu>
2020-06-03 22:48:42 -0700

    need to scrape github pinned myself

--- assets/js/card.js
+++ assets/js/card.js
@@ -1,9 +1,17 @@
 fetch('https://gh-pinned-repos.now.sh/?username=yuchenliu15')
     .then(res => res.json())
     .then(data => {
-        const node = document.getElementsByClassName("card-container");
+        const container = document.getElementsByClassName("container")[0];
+        const cardContainer = document.createElement("div");
+        cardContainer.setAttribute('class', 'card-container');
+        container.appendChild(cardContainer);
+
         for (let index in data) {
-            node[0].innerHTML += data[index].repo + "\n";
+            const title = data[index].repo;
+            const div = document.createElement("div");
+            div.setAttribute('class', 'card');
+            div.textContent = title;
+            cardContainer.appendChild(div);
         }
 
     })
